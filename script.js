385e4489e7d8c301109f1112c423a6bf869dde23
Pragati Aliani <pragatialiani2003@gmail.com>
2023-10-02 15:08:59 +0530

    scroll trigger added

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const timeline = gsap.timeline({ defaults: { duration: 1 } });
+gsap.registerPlugin(ScrollTrigger);
+
+const timeline = gsap.timeline({
+    defaults: { duration: 1 },
+});
 timeline
     .from(".header", { y: "-100%", ease: "bounce" })
     .from(".link", { opacity: 0, stagger: 0.5 })
@@ -11,9 +15,34 @@ timeline
         { opacity: 1, scale: 1, rotation: 0 }
     );
 
+const boxtimeline = gsap.timeline({
+    scrollTrigger: {
+        scroller: ".main",
+        trigger: ".animate-me",
+        start: "center 70%",
+        end: "center 30%",
+        scrub: true,
+        markers: true,
+    },
+    defaults: { duration: 1 },
+});
+
+boxtimeline.fromTo(
+    ".animate-me",
+    {
+        backgroundColor: "pink",
+        height: "20%",
+    },
+    {
+        backgroundColor: "black",
+        height: "60%",
+    }
+);
+
 const button = document.querySelector(".button");
 
 button.addEventListener("click", () => {
     timeline.timeScale(3);
     timeline.reverse();
+    document.querySelector(".main").style.display = "none"; 
 });
